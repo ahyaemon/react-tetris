42f56f14440aefe0f8b07094972a9683a0b4f2d1
ahyaemon <iamahyaemon@gmail.com>
2021-05-03 09:49:01 +0900

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import './App.scss';
+import Screen from "./components/screen/Screen";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
-  );
+    return (
+        <div className="app">
+            <header className="app__header">tetris simulator</header>
+            <div className="app__screen">
+                <Screen/>
+            </div>
+            <div className="app_controller">
+                controller
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/screen/Screen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Screen.scss';
+
+export default function Screen() {
+    return (
+        <div className="screen">
+            <div className="screen__left">
+                screen__left
+            </div>
+            <div className="screen__right">
+                screen__right
+            </div>
+        </div>
+    )
+}
