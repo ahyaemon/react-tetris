a5aba134932c425630f7670bc1148146ddbb5722
ahyaemon <iamahyaemon@gmail.com>
2021-05-03 09:56:37 +0900

    update screen

--- src/components/screen/Screen.tsx
+++ src/components/screen/Screen.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Screen.scss';
+import Left from "./left/Left";
+import Right from "./right/Right";
 
 export default function Screen() {
     return (
         <div className="screen">
             <div className="screen__left">
-                screen__left
+                <Left/>
             </div>
             <div className="screen__right">
-                screen__right
+                <Right/>
             </div>
         </div>
     )
---
+++ src/components/screen/left/Left.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Left.scss';
+
+export default function Left() {
+    return (
+        <div className="left">
+            <div className="left__next">
+                next
+            </div>
+            <div className="left__board">
+                board
+            </div>
+        </div>
+    )
+}
