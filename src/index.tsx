54c82c22413d3112a207370d2d4de3daec6e5602
ahyaemon <iamahyaemon@gmail.com>
2022-05-04 13:54:16 +0900

    React18 で render の呼び出し方法が変わったからその対応

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {RecoilRoot} from "recoil";
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
-      <RecoilRoot>
-          <App />
-      </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
