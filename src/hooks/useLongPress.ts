08058de0671f85673af1c26d24e19a3e713cbb26
ahyaemon <iamahyaemon@gmail.com>
2022-05-16 16:07:29 +0900

    スマホでページ切り替えができないバグを修正

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -6,11 +6,19 @@ const firstTouchMilliSeconds = 300
 const intervalMilliSeconds = 80
 
 export const useLongPressMobile = (command: Command) => {
+    console.log('use long press mobile')
     const { updateRecentlyGame } = useGameHistory()
     let intervalRef: any = useRef(null)
     let timeoutRef: any = useRef(null)
 
     return useCallback((target: any) => {
+        // react router による画面切り替え時にもこの callback が呼ばれてしまうため、
+        // null の場合に即リターンすることによって対処する
+        // この方法は多分あんまりよくない
+        if (target === null) {
+            return
+        }
+
         target.addEventListener('touchstart', (e: any) => {
             e.preventDefault()
 
