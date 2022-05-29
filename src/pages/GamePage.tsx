7153bcb73d39a4aa4ab595fea62dc947915312e1
ahyaemon <iamahyaemon@gmail.com>
2022-05-30 08:47:58 +0900

    キーボード操作説明に回転を追加

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -143,6 +143,7 @@ export function GamePage() {
                     <div>
                         <p>キーボード操作</p>
                         <p>移動 : [←][↓][→] or [a][s][d]</p>
+                        <p>回転 : [z][x] or [k][l]</p>
                         <p>ハードドロップ : [↑] or [w]</p>
                         <p>ホールド : [Shift]</p>
                     </div>
