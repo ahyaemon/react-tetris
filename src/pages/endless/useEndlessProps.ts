afe951905abd256678e6ec823bb987aec488a08b
ahyaemon <iamahyaemon@gmail.com>
2022-07-21 08:54:01 +0900

    GameStore を削除

--- src/pages/endless/useEndlessProps.ts
+++ src/pages/endless/useEndlessProps.ts
@@ -1,10 +1,7 @@
 import {Game} from "../../game/game";
-import {createGameStore} from "../../stores/GameStore";
 import {atom, useRecoilState} from "recoil";
 import {Command} from "../../game/command";
 import {Endless} from "../../game/Endless";
-
-const endlessStore = createGameStore("endless", Game.create(Math.random() * 1000000))
 
 const endlessState= atom({
     key: 'endless',
