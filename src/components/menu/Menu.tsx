07d1e25ffb3cdb343a7eb666886aef4265ae5f93
ahyaemon <iamahyaemon@gmail.com>
2022-07-24 15:05:46 +0900

    テンプレ練習に DT 砲を追加

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -15,7 +15,16 @@ function LinkWithClose(props: LinkWithCloseProps) {
     return <div><Link to={props.to} onClick={ () => {props.setIsOpen(false)} } >{ props.title }</Link></div>
 }
 
-const practiceParameter = "v=1&p=eJyNjUsOwkAMQ%2B%2BSdYOcTD5ML9NDwA5xd9JpVVGpSHhhvYX1%2FKIHzT3CYbd7a%2BgZPXKiJ80EICKqwQK0FrZjsZk7iyArhcuPraoeW%2F3abkk%2BY%2F3DPdkGx0BbvRVhseEVkT8M43g36IWB3h%2FnTDKU"
+const practiceParameters: { title: string, queryString: string }[] = [
+    {
+        title: "TSD → TSD",
+        queryString: "v=1&p=eJyNjUsOwkAMQ%2B%2BSdYOcTD5ML9NDwA5xd9JpVVGpSHhhvYX1%2FKIHzT3CYbd7a%2BgZPXKiJ80EICKqwQK0FrZjsZk7iyArhcuPraoeW%2F3abkk%2BY%2F3DPdkGx0BbvRVhseEVkT8M43g36IWB3h%2FnTDKU",
+    },
+    {
+        title: "DT砲",
+        queryString: "v=1&p=eJytjTsSAkEIBe9C7GwBj4%2FuZTyEZtbeXWacXRNNLAmoDqD7QTdaxczzvMAvcFWN9BPdaSXuY301ZhE2eyELUJgtgkUB90Lmes2JdZpANvciyQA6ihRGXH%2F1Fhxe%2Febtk20PTByFHLXiLpu14f1Y%2B4dXD6%2Fq26u7l7Ynk4pMwA%3D%3D",
+    }
+]
 
 export function Menu() {
 
@@ -28,9 +37,26 @@ export function Menu() {
             })}
         >
             <BurgerMenu width="280px" isOpen={isOpen} onOpen={() => setIsOpen(true)} onClose={() => setIsOpen(false)}>
-                <LinkWithClose to="/" title="エンドレス" setIsOpen={setIsOpen}/>
-                <LinkWithClose to={`/practice?${practiceParameter}`} title="テンプレ練習" setIsOpen={setIsOpen}/>
-                <LinkWithClose to="/about" title="About" setIsOpen={setIsOpen}/>
+                <ul>
+                    <li>
+                        <LinkWithClose to="/" title="エンドレス" setIsOpen={setIsOpen}/>
+                    </li>
+                    <li>
+                        テンプレ練習
+                        <ul>
+                            {
+                                practiceParameters.map(p => (
+                                    <li key={p.title}>
+                                        <LinkWithClose to={`/practice?${p.queryString}`} title={p.title} setIsOpen={setIsOpen}/>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </li>
+                    <li>
+                        <LinkWithClose to="/about" title="About" setIsOpen={setIsOpen}/>
+                    </li>
+                </ul>
             </BurgerMenu>
         </div>
     )
