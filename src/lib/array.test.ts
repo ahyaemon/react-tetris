669d6e53aa0c7d33a3bf42a7433d64c1025cd66e
ahyaemon <iamahyaemon@gmail.com>
2022-08-02 00:28:06 +0900

    配列用の util メソッド追加

--- src/lib/array.test.ts
+++ src/lib/array.test.ts
@@ -1,4 +1,4 @@
-import {lastOf} from "./array";
+import {lastOf, removeFirstOf, removeLastOf} from "./array";
 
 describe("lastIOf", () => {
 
@@ -7,7 +7,31 @@ describe("lastIOf", () => {
         expect(lastOf(["a", "b", "c"])).toBe("c")
     })
 
-    test("配列が空の時は undefined", () => {
-        expect(lastOf([])).toBeUndefined()
+    test("配列が空の時はエラー", () => {
+        expect(() => {lastOf([])}).toThrow(Error)
+    })
+})
+
+describe("removeFirstOf", () => {
+
+    test("配列が空じゃない時は、最初の要素を削除して残りの要素が取れる", () => {
+        expect(removeFirstOf([1, 2, 3])).toStrictEqual([2, 3])
+    })
+
+    test("配列が空の時は空の配列が返ってくる", () => {
+        expect(removeFirstOf([])).toStrictEqual([])
+    })
+})
+
+describe("removeLastOf", () => {
+
+    test("配列が空じゃない時は、最後の n 個の要素を削除して残りの要素が取れる", () => {
+        expect(removeLastOf([1, 2, 3], 1)).toStrictEqual([1, 2])
+        expect(removeLastOf([1, 2, 3], 2)).toStrictEqual([1])
+    })
+
+    test("配列が空の時は空の配列が返ってくる", () => {
+        expect(removeLastOf([], 1)).toStrictEqual([])
+        expect(removeLastOf([], 2)).toStrictEqual([])
     })
 })
--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,3 +1,29 @@
-export function lastOf<T>(ar: T[]): T | undefined {
+/**
+ * 配列の最後の要素を返す。
+ * 配列が空の場合は Error を投げる。
+ * @param ar
+ */
+export function lastOf<T>(ar: T[]): T {
+    if (ar.length === 0) {
+        throw Error("空の配列から要素を取得することはできない")
+    }
     return ar.slice(-1)[0]
+}
+
+/**
+ * 配列の最初の要素を削除し、残りの配列を返す。
+ * 配列が空の場合は空の配列を返す。
+ * @param ar
+ */
+export function removeFirstOf<T>(ar: T[]): T[] {
+    return ar.slice(1)
+}
+
+/**
+ * 配列の最後から n 個の要素を削除し、残りの配列を返す。
+ * 配列が空の場合は空の配列を返す。
+ * @param ar
+ */
+export function removeLastOf<T>(ar: T[], n: number): T[] {
+    return ar.slice(0, -n)
 }
